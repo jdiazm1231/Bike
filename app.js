1ecde773afde0620007142c22f32b61e27b8b6b6
Jonathan <jdiazm3112@gmail.com>
2016-07-11 23:12:34 -0500

    En esta oportunidad Creamos el formulario basico de creacion de productos con un nomre,precio,categoria,descripcion tulizamos el layout de jade para ahorrar codigo y con bodyparser accedimos a los atributos de nuestro formulario

--- app.js
+++ app.js
@@ -1,41 +1,62 @@
 var express = require('express'); // importo express framework
 var mongoose = require('mongoose'); // Driver de Mongo DB
+var bodyParser = require('body-parser'); // Nos Parsea eL contenido del Formulario
 var app = express(); //asigno express a una variable
 
-mongoose.connect("mongodb://localhost/bike");//Conexion con la Base De Datos
+mongoose.connect("mongodb://localhost/bike"); //Conexion con la Base De Datos
+
 
 //Definimos Nuestro Schema
-
 var productosSchema = {
 
-	nombre : String,
-	categoria : String,
-	precio : Number
+	nombre: String,
+	categoria: String,
+	precio: Number,
+	descripcion: String
 
 };
 
 //Creamos Nuestro Modelo 
-var ProductModel = mongoose.model("Product",productosSchema);
+var ProductModel = mongoose.model("Product", productosSchema);
 
-/*
-var data = {
+app.set("view engine", "jade"); //defino jade como engine de las vistas
+app.use(express.static("public")); //definimos mi Ruta Estatica
+app.use(bodyParser.json()); //Necesario de Body-Parser
+app.use(bodyParser.urlencoded({
+	extended: true
+})); //Necesario de Body-Parser
 
-	nombre: "Zapatillas Ruta",
-	categoria: "Ruta",
-	precio: 120000
-}
 
-var prd = new ProductModel(data);
+app.post("/product", function(require, response) {
 
-prd.save(function(err){
-	console.log(prd);
-})
-*/
 
-app.set("view engine","jade"); //defino jade como engine de las vistas
-app.use(express.static("public"));//deifinimos mi Ruta Estatica
+	var data = {
 
-app.get("/",function(require,response){
+		nombre: require.body.nombre,
+		categoria: require.body.categoria,
+		precio: require.body.precio,
+		descripcion: require.body.descripcion
+	}
+
+	var prd = new ProductModel(data);
+
+	prd.save(function(err) {
+		console.log(prd);
+		response.render("index");
+	});
+
+
+});
+
+// ruta para formulario Nuevo Producto Renderizamos vista
+app.get("/new", function(require, response) {
+
+	response.render("new");
+
+});
+
+//ruta para el index Renderizamos vista
+app.get("/", function(require, response) {
 
 	console.log('corriendo');
 	response.render("index");
@@ -43,4 +64,5 @@ app.get("/",function(require,response){
 
 });
 
+//Puerto de escucha del Server
 app.listen(8080);
