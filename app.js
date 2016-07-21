f2d41a7e457bd4a017d7a6dd1a537938219446b5
Jonathan <jdiazm3112@gmail.com>
2016-07-20 20:28:36 -0500

    Modificamos la forma de modificar las imagenes y la forma de ver los registros que no tiene imagenes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express'); // importo express framework
 var mongoose = require('mongoose'); // Driver de Mongo DB
 var bodyParser = require('body-parser'); // Nos Parsea eL contenido del Formulario
-var multer = require('multer');
+var multer = require('multer'); // multer es para poder subir archivos al server se manera facil
 var app = express(); //asigno express a una variable
 var override = require('method-override');
 mongoose.connect("mongodb://localhost/bike"); //Conexion con la Base De Datos
@@ -18,7 +18,7 @@ var productosSchema = {
 
 };
 
-//Creamos Nuestro Modelo 
+//Creamos Nuestro Modelo
 var ProductModel = mongoose.model("Product", productosSchema);
 
 app.set("view engine", "jade"); //defino jade como engine de las vistas
@@ -33,7 +33,7 @@ app.use(multer({
 }).single('inputFileName'));
 
 
-
+//Creamos Ruta para mostrar todos los productos actuales
 app.get("/admin", function(require, response) {
 
 	ProductModel.find(function(err, productos) {
@@ -48,6 +48,7 @@ app.get("/admin", function(require, response) {
 	});
 });
 
+//Traemos todos los Datos  de un producto y renderizamos para proceder a  Modificar
 app.get("/admin/edit/:id", function(require, response) {
 
 	var product_id = require.params.id;
@@ -67,15 +68,27 @@ app.get("/admin/edit/:id", function(require, response) {
 
 });
 
+//Actualizamos el producto que antes le habiamos rellenado los Datos
 app.put("/admin/:id", function(require, response) {
 
-	var data = {
+	if (require.file) {
+		var data = {
 
-		nombre: require.body.nombre,
-		categoria: require.body.categoria,
-		precio: require.body.precio,
-		descripcion: require.body.descripcion,
-		//img: require.file.path
+			nombre: require.body.nombre,
+			categoria: require.body.categoria,
+			precio: require.body.precio,
+			descripcion: require.body.descripcion,
+			img: require.file.filename
+		}
+	} else {
+		var data = {
+
+			nombre: require.body.nombre,
+			categoria: require.body.categoria,
+			precio: require.body.precio,
+			descripcion: require.body.descripcion,
+			img: 'default.jpg'
+		}
 	}
 
 	ProductModel.update({
@@ -88,6 +101,8 @@ app.put("/admin/:id", function(require, response) {
 
 });
 
+/*Eliminamos un producto dependiendo el id que tengamos estamos haciendolo por metodo .get lo conveniente es
+ hacerlo por metodo .delete */
 app.get("/delete/:id", function(require, response) {
 
 	ProductModel.remove({
@@ -105,21 +120,31 @@ app.get("/delete/:id", function(require, response) {
 
 });
 
+//Guardamos el producto subiendo la imagen  pero antes tenemos que crear el modelo de mongodb
 app.post("/product", function(require, response) {
 
+	if (require.file) {
+		var data = {
 
-	var data = {
+			nombre: require.body.nombre,
+			categoria: require.body.categoria,
+			precio: require.body.precio,
+			descripcion: require.body.descripcion,
+			img: require.file.filename
+		}
+	} else {
+		var data = {
 
-		nombre: require.body.nombre,
-		categoria: require.body.categoria,
-		precio: require.body.precio,
-		descripcion: require.body.descripcion,
-		img: require.file.filename
+			nombre: require.body.nombre,
+			categoria: require.body.categoria,
+			precio: require.body.precio,
+			descripcion: require.body.descripcion,
+			img: 'default.jpg'
+		}
 	}
 
 	var prd = new ProductModel(data);
 	console.log(require.file);
-
 
 	prd.save(function(err) {
 		console.log(prd);
@@ -136,20 +161,22 @@ app.get("/new", function(require, response) {
 
 });
 
+// Listamos Todos los Productos EXistentes solo como visualizacion
 app.get("/product", function(require, response) {
 
-		ProductModel.find(function(err, productos) {
+	ProductModel.find(function(err, productos) {
 
-			if (err) {
-				console.log(err);
-			}
-			response.render("productos", {
-				products: productos
-			});
-
+		if (err) {
+			console.log(err);
+		}
+		response.render("productos", {
+			products: productos
 		});
-	})
-	//ruta para el index Renderizamos vista
+
+	});
+});
+
+//ruta para el index Renderizamos vista
 app.get("/", function(require, response) {
 
 	console.log('corriendo');
